4cda503a6a4991b90f7fab5ff76569840b94a09d
Ryan Gentzler <ryan.gentzler@eagles.oc.edu>
2019-03-28 20:57:24 -0500

    Canvas Drawn

--- Drawing/DrawingUI.h
+++ Drawing/DrawingUI.h
@@ -10,19 +10,30 @@ using namespace sf;
 class DrawingUI
 {
 private:
-
+	RectangleShape canvas;
 public:
 	DrawingUI(Vector2f p)
 	{
+		Vector2f sqPos(p);
+		canvas.setPosition(sqPos);
+		canvas.setOutlineColor(Color::White);
+		canvas.setOutlineThickness(2);
+		canvas.setSize(Vector2f(550, 500));
+		canvas.setFillColor(Color::White);
+
 	}
 
 	void draw(RenderWindow& win, ShapeMgr *mgr)
 	{
+		win.draw(canvas);
 	}
 	
 	bool isMouseInCanvas(Vector2f mousePos)
 	{
-		return false; // just to make it compile
+		bool temp;
+		if (canvas.getGlobalBounds().contains(mousePos))
+			temp = true;
+		return temp; // just to make it compile
 	}
 
 };
